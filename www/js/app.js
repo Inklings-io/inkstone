3da4b16af50bc529ed2b9ef5558695989d3acb74
BenRoberts <ben@cs.uri.edu>
2015-01-12 08:22:18 -0500

    removing all non-free icons

--- www/js/app.js
+++ www/js/app.js
@@ -5,6 +5,7 @@
     var loginTpl = Handlebars.compile($("#login-tpl").html());
     var homeTpl = Handlebars.compile($("#home-tpl").html());
     var newNoteTpl = Handlebars.compile($("#new-note-tpl").html());
+    var newReplyTpl = Handlebars.compile($("#new-reply-tpl").html());
     var newCheckinTpl = Handlebars.compile($("#new-checkin-tpl").html());
     var cameraTpl = Handlebars.compile($("#camera-tpl").html());
     var audioTpl = Handlebars.compile($("#audio-tpl").html());
@@ -64,26 +65,27 @@
     /* ---------------------------------- Local Functions ---------------------------------- */
     function renderHomeView() {
         icons = [];
-        icons.push({'image':'icons-nonfree/iconSweets2/'+config['palette']+'/64-Speech-Bubble.png', 'label':'Note', 'id':'newnote'});
-        icons.push({'image':'icons-nonfree/iconSweets2/'+config['palette']+'/64-Marker.png', 'label':'Checkin', 'id':'newcheckin'});
+        icons.push({'image':'svg/bubble.png', 'label':'Note', 'id':'newnote'});
+        icons.push({'image':'svg/bubbles.png', 'label':'Reply', 'id':'newreply'});
+        icons.push({'image':'svg/marker.png', 'label':'Checkin', 'id':'newcheckin'});
 
         if(config['support_photo']){
-            icons.push({'image':'icons-nonfree/iconSweets2/'+config['palette']+'/64-Camera.png', 'label':'Photo', 'id':'newphoto'});
+            icons.push({'image':'svg/camera.png', 'label':'Photo', 'id':'newphoto'});
         }
         if(config['support_audio']){
-            icons.push({'image':'icons-nonfree/iconSweets2/'+config['palette']+'/64-Microphone.png', 'label':'Audio', 'id':'newaudio'});
+            icons.push({'image':'svg/microphone.png', 'label':'Audio', 'id':'newaudio'});
         }
         if(config['support_video']){
-            icons.push({'image':'icons-nonfree/iconSweets2/'+config['palette']+'/64-Film-Strip.png', 'label':'Video', 'id':'newvideo'});
+            icons.push({'image':'svg/film.png', 'label':'Video', 'id':'newvideo'});
         }
 
-        //icons.push({'image':'icons-nonfree/iconSweets2/'+config['palette']+'/64-Day-Calendar.png', 'label':'New Event', 'id':'newevent'});
-        icons.push({'image':'icons-nonfree/iconSweets2/'+config['palette']+'/64-Settings-2.png', 'label':'Settings', 'id':'settings'});
-        icons.push({'image':'icons-nonfree/iconSweets2/'+config['palette']+'/64-Exit.png', 'label':'Logout', 'id':'exit'});
+        //icons.push({'image':'svg/daycalendar.png', 'label':'New Event', 'id':'newevent'});
+        icons.push({'image':'svg/gear.png', 'label':'Settings', 'id':'settings'});
+        icons.push({'image':'svg/power.png', 'label':'Logout', 'id':'exit'});
         
-        //icons.push({'image':'icons-nonfree/iconSweets2/'+config['palette']+'/64-Document.png', 'label':'New Article', 'id':'newarticle'});
         $('body').html(homeTpl(icons));
         $('#homeicon_newnote').on('click', renderNewNoteView);
+        $('#homeicon_newreply').on('click', renderNewReplyView);
         $('#homeicon_newcheckin').on('click', renderNewCheckinView);
         $('#homeicon_exit').on('click', logout);
         $('#homeicon_newphoto').on('click', renderNewPhotoView);
@@ -220,9 +222,28 @@
         });
     }
 
+    function renderNewReplyView() {
+        $('body').html(newReplyTpl(config));
+        $('#home-btn').on('click', renderHomeView);
+        $('#post-btn').on('click', sendNote);
+        getSyndicationTargets(function(targets){
+            targets_array = targets.split(',');
+            if(!targets_array){
+                $('#input-syndication-wrapper').hide();
+            } else {
+                $('#input-syndicateto').html('');
+                for (i = 0; i < targets_array.length; i++) {
+                    $('#input-syndicateto').append('<option value="'+targets_array[i]+'">'+targets_array[i]+'</option');
+                }
+                $('#input-syndication-wrapper').show();
+            }
+        });
+    }
+
     function sendCheckin(){
         content = $('#input-content').val();
         syndicate = $('#input-syndicateto').val();
+        category = $('#category').val();
         geo_location = $('#geoloc').val();
         geo_place_name = $('#loc_name').val();
 
@@ -234,6 +255,11 @@
         if(geo_place_name != ''){
             data += '&place_name='+geo_place_name
         }
+
+        if(category){
+            data += '&category=' + encodeURIComponent(category);
+        }
+
         if(syndicate){
             for (i = 0; i < syndicate.length; i++) {
                     data += '&syndicate-to[]='+syndicate[i];
@@ -243,6 +269,8 @@
         mp_send(data, function(){
             alert('Posted!');
             $('#input-content').val('');
+            $('#inreplyto').val('');
+            $('#category').val('');
             $('#input-syndicateto').val('');
             $('#geoloc').val('');
             $('#loc_name').val('');
@@ -258,9 +286,20 @@
     function sendNote(){
         content = $('#input-content').val();
         syndicate = $('#input-syndicateto').val();
+        replyurl = $('#inreplyto').val();
+        category = $('#category').val();
 
         // todo escapte content and syndicate
-        data = 'type=note&h=entry&operation=create&content='+content;
+        data = 'type=reply&h=entry&operation=create&content='+encodeURIComponent(content);
+        
+        if(category){
+            data += '&category=' + encodeURIComponent(category);
+        }
+
+        if(replyurl){
+            data += '&in-reply-to=' + encodeURIComponent(replyurl);
+        }
+
         if(syndicate){
             for (i = 0; i < syndicate.length; i++) {
                     data += '&syndicate-to[]='+syndicate[i];
@@ -270,6 +309,8 @@
         mp_send(data, function(){
             alert('Posted!');
             $('#input-content').val('');
+            $('#inreplyto').val('');
+            $('#category').val('');
             $('#input-syndicateto').val('');
             //$('#success').html('Posted!');
             //$('#success').show();
@@ -363,11 +404,16 @@
     function postVideo(){
         content = $('#input-content').val();
         syndicate = $('#input-syndicateto').val();
+        category = $('#category').val();
 
         data_obj = { 'h': 'entry',
             'type':'video',
             'operation':'create',
             'content': content
+        }
+
+        if(category){
+            data_obj['category'] = encodeURIComponent(category);
         }
         // todo escape content and syndicate
         if(syndicate){
@@ -379,6 +425,8 @@
             function(r){ 
                 alert('Posted!');
                 $('#input-content').val('');
+            $('#inreplyto').val('');
+            $('#category').val('');
                 $('#input-syndicateto').val('');
                 videoData = null;
                 $('#videoFile').html('');
@@ -390,11 +438,15 @@
     function postAudio(){
         content = $('#input-content').val();
         syndicate = $('#input-syndicateto').val();
+        category = $('#category').val();
 
         data_obj = { 'h': 'entry',
             'type':'audio',
             'operation':'create',
             'content': content
+        }
+        if(category){
+            data_obj['category'] = encodeURIComponent(category);
         }
         // todo escape content and syndicate
         if(syndicate){
@@ -406,6 +458,8 @@
             function(r){ 
                 alert('Posted!');
                 $('#input-content').val('');
+            $('#inreplyto').val('');
+            $('#category').val('');
                 $('#input-syndicateto').val('');
                 audioData = null;
                 $('#AudioFile').html('');
@@ -418,11 +472,15 @@
         content = $('#input-content').val();
         photo_uri = photoData;
         syndicate = $('#input-syndicateto').val();
+        category = $('#category').val();
 
         data_obj = { 'h': 'entry',
             'type':'photo',
             'operation':'create',
             'content': content
+        }
+        if(category){
+            data_obj['category'] = encodeURIComponent(category);
         }
         // todo escape content and syndicate
         if(syndicate){
@@ -434,6 +492,8 @@
             function(r){ 
                 alert('Posted!');
                 $('#input-content').val('');
+            $('#inreplyto').val('');
+            $('#category').val('');
                 $('#input-syndicateto').val('');
                 photoData = null;
                 $('#PhotoPreview').attr('src', '');
