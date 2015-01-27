12d6f0608697e741a5274d0a8e87f0a311ac19bc
BenRoberts <ben@cs.uri.edu>
2015-01-26 19:58:22 -0500

    update licensing info

--- www/svg/build.sh
+++ www/svg/build.sh
@@ -1,1 +1,1 @@
-for f in *.svg; do convert +antialias  -background none -density 200 $f `echo $f |sed s/svg/png/`; done
+for f in src/*.svg; do convert +antialias  -background none -density 200 $f `echo $f |sed s/src\\\/// |sed s/svg/png/`; done
---
+++ www/svg/src/readme.txt
@@ -0,0 +1,2 @@
+svg files in this folder were from raphaeljs.com and as such have their own
+license.  You can view these files at http://raphaeljs.com/icons/
