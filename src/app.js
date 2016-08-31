0a52846d5b3eaeb840762db42722cc27784f4327
Ben Roberts <ben@directdigitalllc.com>
2016-08-31 16:05:07 -0400

    work on porting pieces from old code base

--- src/app.js
+++ src/app.js
@@ -1,5 +1,18 @@
+import {Router, RouterConfiguration} from 'aurelia-router';
+
 export class App {
-  constructor() {
-    this.message = 'Hello World!';
+  router: Router;
+
+  configureRouter(config: RouterConfiguration, router: Router){
+    config.title = 'Mobilepub';
+    config.map([
+      { route: '',           moduleId: 'home',   	  title: 'Home'},
+      { route: 'login',      moduleId: 'login',  	  title: 'Login'},
+      { route: 'settings',   moduleId: 'settings',    title: 'Settings'},
+      { route: 'post/:num',  moduleId: 'post-detail', name:'post' },
+      { route: 'post/new',   moduleId: 'post-detail', name:'newpost' }
+    ]);
+
+    this.router = router;
   }
 }
---
+++ src/home.js
@@ -0,0 +1,27 @@
+export class Home{
+
+        icons = [
+            {'image':'icons/circle-icons/pencil.svg', 'label':'Note', 'id':'newnote'}, 
+            {'image':'icons/circle-icons/bubble.svg', 'label':'Reply', 'id':'newreply'},
+            {'image':'icons/circle-icons/location.svg', 'label':'Checkin', 'id':'newcheckin'},
+            {'image':'icons/circle-icons/bookmark.svg', 'label':'Bookmark', 'id':'newbookmark'},
+            {'image':'icons/circle-icons/heart.svg', 'label':'Like', 'id':'newlike'},
+            {'image':'icons/circle-icons/power.svg', 'label':'Logout', 'id':'exit'}
+        ];
+
+        /*
+        if(config['support_photo']){
+            icons.push({'image':'icons/circle-icons/photo.svg', 'label':'Photo', 'id':'newphoto'});
+        }
+        if(config['support_audio']){
+            icons.push({'image':'icons/circle-icons/mic.svg', 'label':'Audio', 'id':'newaudio'});
+        }
+        if(config['support_video']){
+            icons.push({'image':'icons/circle-icons/film.svg', 'label':'Video', 'id':'newvideo'});
+        }
+        */
+
+        //icons.push({'image':'icons/circle-icons/calendar.svg', 'label':'New Event', 'id':'newevent'});
+        //icons.push({'image':'icons/circle-icons/settings.svg', 'label':'Settings', 'id':'settings'});
+
+}
