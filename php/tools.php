edb81d495fd3ed6bf95fcd461de7608cffcb0fa0
Ben Roberts <ben@thatmustbe.me>
2016-12-05 09:00:17 -0500

    make safe bondary data

--- php/tools.php
+++ php/tools.php
@@ -22,10 +22,15 @@ function uploadToMediaEndpoint($media_endpoint, $bearer_string, $media_data){
 	$filesize = $media_data['size'];
 	if ($filedata != '')
 	{
-        echo "debug 10\n";
 
 //TODO make this safe
 $boundary = md5($filedata);
+
+// generate safe boundary
+    do {
+        $boundary = "---------------------" . md5(mt_rand() . microtime());
+    } while (preg_grep("/{$boundary}/", $filename.$filedata));
+    
 
         $headers = array('Content-Type:multipart/form-data; boundary='.$boundary,
                          'Authorization: ' . $bearer_string
@@ -49,17 +54,13 @@ Content-Transfer-Encoding: binary
 			CURLOPT_POSTFIELDS => $postfields,
 			CURLOPT_RETURNTRANSFER => true
 		); // cURL options
-        echo "debug 40\n";
 		curl_setopt_array($ch, $options);
-        echo "debug 50\n";
+
 		$response = curl_exec($ch);
-        echo "$media_endpoint\n";
-        print_r($response);
-        echo "debug 60\n";
+
 		if(!curl_errno($ch)) {
 			$info = curl_getinfo($ch);
 			if ($info['http_code'] == 201){
-        echo "debug 70\n";
 				$errmsg = "File uploaded successfully";
 				$header_text = substr($response, 0, strpos($response, "\r\n\r\n"));
 				$body_text = substr($response, strlen($header_text));
@@ -67,7 +68,6 @@ Content-Transfer-Encoding: binary
 				foreach (explode("\r\n", $header_text) as $i => $line){
 
 					if( preg_match('/^location/i', $line)){
-        echo "debug 75\n";
 
 						$split = explode(': ', $line);
 						curl_close($ch);
@@ -76,15 +76,12 @@ Content-Transfer-Encoding: binary
 					}
 				}
 			} else {
-        echo "debug 80\n";
 				// todo if ! 201
 				curl_close($ch);
 				return null;
 			}
 		} else {
-        echo "debug 90\n";
 			$errmsg = curl_error($ch);
-        echo $errmsg;
 			curl_close($ch);
 			return null;
 		}
